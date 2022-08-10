e3b413246abbacfafc5e5f3ccee0131f046af69e
Tafriyadi27 <tafriyadi273@gmail.com>
2022-08-10 18:08:23 +0700

    make shared prefereance(save username) in UserActivity and show in MainActivity

--- app/src/main/java/com/skullbreraker/tebakgambarvtuber/MainActivity.java
+++ app/src/main/java/com/skullbreraker/tebakgambarvtuber/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnexit = findViewById(R.id.btnexit);
+        SharedPreferences sharedPreferences = getSharedPreferences("user", MODE_PRIVATE);
+        String user = sharedPreferences.getString("user", "");
+        if (user.isEmpty()) {
+            TextView textView = findViewById(R.id.txtUser);
+            textView.setText("Nama User : Guest");
+        } else
+        {
+            TextView textView = findViewById(R.id.txtUser);
+            textView.setText("Nama User : " + user);
+        }
+        ButterKnife.bind(this);
         btnexit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/skullbreraker/tebakgambarvtuber/UserActivity.java
+++ app/src/main/java/com/skullbreraker/tebakgambarvtuber/UserActivity.java
@@ -3,20 +3,34 @@ package com.skullbreraker.tebakgambarvtuber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.BreakIterator;
+
 
 public class UserActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         Button btnstart = findViewById(R.id.btnStart);
+        EditText editText = findViewById(R.id.edtUser);
         btnstart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences("user", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putString("user", editText.getText().toString());
+                editor.apply();
+
                 Intent intent = new Intent(UserActivity.this, MainActivity.class);
                 startActivity(intent);
             }
