15f9bb9ea725083b3bbb3b4acd655fe758c54663
Tafriyadi27 <tafriyadi273@gmail.com>
2022-08-12 20:53:51 +0700

    change base url in apiendpoint

--- app/src/main/java/com/skullbreraker/tebakgambarvtuber/api/apiendpoint.java
+++ app/src/main/java/com/skullbreraker/tebakgambarvtuber/api/apiendpoint.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class apiendpoint {
-    private static final String BASE_URL = "http://192.168.100.3:5000";
+    private static final String BASE_URL = "https://trizib.my.id";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
